7aacc6c7a3f4b1250d6f6b71ea33e34865dc159d
ryosukes <nu.ciger@gmail.com>
2018-02-12 20:29:00 +0900

    fix to create endpoint

--- hello/main.go
+++ hello/main.go
@@ -1,17 +1,16 @@
 package main
 
 import (
+    "fmt"
+    "github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-type Response struct {
-	Message string `json:"message"`
-}
 
-func Handler() (Response, error) {
-	return Response{
-		Message: "Go Serverless v1.0! Your function executed successfully!",
-	}, nil
+func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	fmt.Println("Received body: ", request.Body)
+
+	return events.APIGatewayProxyResponse{Body: request.Body, StatusCode: 200}, nil
 }
 
 func main() {
